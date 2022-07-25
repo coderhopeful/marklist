8f05c94749e3fa5ea7167bd757a48fe670f58425
Arjun <emailarjunmuraly@gmail.com>
2022-07-25 15:27:51 +0530

    styles added

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import {useNavigate} from "react-router-dom"
-
+import "./homepage.css"
 import { useState, useEffect } from 'react'
 import { useParams,Link} from 'react-router-dom'
 
@@ -61,21 +61,21 @@ else{
     const student = marklist.find((i) => i.id== id)
 
     return (
-        <div>
+        <div className="container">
 {student? (
 
-<Card style={{ width: '18rem' }}>
-<Card.Header>Name: {student.name}</Card.Header>
+<Card  className="card" style={{ width: '18rem' }}>
+<Card.Header><span>Name:</span> {student.name}</Card.Header>
 <ListGroup variant="flush">
-  <ListGroup.Item>Date of Birth: {student.dob} </ListGroup.Item>
-  <ListGroup.Item>Marklist </ListGroup.Item>
+  <ListGroup.Item ><span>Date of Birth:</span> {student.dob} </ListGroup.Item>
+  <ListGroup.Item><span className="marklist-title">Marklist</span> </ListGroup.Item>
   <ListGroup.Item>English: {student.marks.English} | Grade: {setGrade(student.marks.English)}</ListGroup.Item>
   <ListGroup.Item>Hindi: {student.marks.Hindi} | Grade: {setGrade(student.marks.Hindi)}</ListGroup.Item>
   <ListGroup.Item>Malayalam: {student.marks.Malayalam} | Grade: {setGrade(student.marks.Malayalam)}</ListGroup.Item>
   <ListGroup.Item>Maths: {student.marks.Maths} | Grade: {setGrade(student.marks.Maths)}</ListGroup.Item>
-  <ListGroup.Item>Total: {student.marks.Maths+student.marks.Malayalam+student.marks.Hindi+student.marks.English}/400</ListGroup.Item>
-  {/* <ListGroup.Item>Total Percentage:{Math.round(((student.marks.Maths+student.marks.Malayalam+student.marks.Hindi+student.marks.English)/400)*100)}%</ListGroup.Item> */}
-  <ListGroup.Item>Total Percentage: {setTotal(student.marks)}</ListGroup.Item>
+  <ListGroup.Item><span>Total Marks:</span> {student.marks.Maths+student.marks.Malayalam+student.marks.Hindi+student.marks.English}/400</ListGroup.Item>
+  
+  <ListGroup.Item><span>Total Percentage:</span> {setTotal(student.marks)}</ListGroup.Item>
 </ListGroup>
 </Card>
 
@@ -83,7 +83,7 @@ else{
 
 ):"null"}
 
-<Link className="btn btn-warning my-2 rounded" to="/">View Another</Link>
+<Link className=" btn btn-outline-dark mt-3 rounded" to="/"><i class="fas fa-home"></i> View Another</Link>
 
         </div>
     )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState,useEffect } from "react";
 import {useNavigate} from "react-router-dom"
+import "./login.css"
 
 
 
@@ -32,11 +33,12 @@ const Login = () => {
 
     return (
         <div className="container">
-            <form onSubmit={handleSubmit}>
+            <form className="login" onSubmit={handleSubmit}>
                 <h1>Student Login</h1>
-                <input type="text" name="name" placeholder="Enter name of student" value={name} onChange={(e) => setName(e.target.value)} className='form-control w-25' />
-                <input type="text" name="dob" placeholder="Enter DOB in dd-mm-yyyy format" value={dob} onChange={(e) => setDOB(e.target.value)} className='form-control w-25' />
-                <button className='btn btn-success' type="submit">Submit</button>
+                
+                <input type="text" name="name" placeholder="Enter name of student" value={name} onChange={(e) => setName(e.target.value)} className='form-control ' />
+                <input type="text" name="dob" placeholder="Enter DOB in dd-mm-yyyy format" value={dob} onChange={(e) => setDOB(e.target.value)} className='form-control ' />
+                <button className='btn btn-outline-success'  type="submit">Submit</button>
             </form>
         </div>
     )
